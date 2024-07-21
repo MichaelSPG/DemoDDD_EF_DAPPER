6a283447823277301b2373fbb1398908e68e7d0b
Michael Steven Patiño Guerra <michael.mspg@gmail.com>
2024-07-21 01:28:25 -0500

    add migration

---
+++ DemoDDD.Infrastucture/Migrations/20240721021332_InitialCreate.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoDDD.Infrastucture.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Names = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(400)", maxLength: 400, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicle",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Vin = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    Address_Country = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address_State = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address_Province = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address_City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address_Street = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Price_CurrencyKind = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MaintenanceAmount_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    MaintenanceAmount_CurrencyKind = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastRentDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Accesories = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Version = table.Column<long>(type: "bigint", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicle", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rental",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    VehicleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Duration_Start = table.Column<DateOnly>(type: "date", nullable: true),
+                    Duration_End = table.Column<DateOnly>(type: "date", nullable: true),
+                    PerPeriodPrice_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    PerPeriodPrice_CurrencyKind = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaintenancePrice_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    MaintenancePrice_CurrencyKind = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Accesories_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Accesories_CurrencyKind = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TotalPrice_Value = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    TotalPrice_CurrencyKind = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ConfirmationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RejectionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CompletionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CancellationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rental", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rental_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rental_Vehicle_VehicleId",
+                        column: x => x.VehicleId,
+                        principalTable: "Vehicle",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Review",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RentId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    Commentary = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Review_Rental_RentId",
+                        column: x => x.RentId,
+                        principalTable: "Rental",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rental_UserId",
+                table: "Rental",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rental_VehicleId",
+                table: "Rental",
+                column: "VehicleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_RentId",
+                table: "Review",
+                column: "RentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Email",
+                table: "User",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Review");
+
+            migrationBuilder.DropTable(
+                name: "Rental");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Vehicle");
+        }
+    }
+}
---
+++ DemoDDD.Infrastucture/Migrations/20240721033343_AccesoriesUpdate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DemoDDD.Infrastucture.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccesoriesUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Version",
+                table: "Vehicle",
+                type: "bigint",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldRowVersion: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Version",
+                table: "Vehicle",
+                type: "bigint",
+                rowVersion: true,
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldRowVersion: true,
+                oldDefaultValue: 0L);
+        }
+    }
+}
